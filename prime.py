a6c68941ece5559b553554229b8f1ee90b079923
gak <g.a.k21@hotmail.com>
2018-03-23 15:53:35 +0700

    Revert "udah diganti coy"
    This reverts commit feb0309a1f5a88374604919228f128d55b671f33.

--- prime.py
+++ prime.py
@@ -1,13 +1,13 @@
 def prime(bil):
     count=0
 
-    # for i in range(1,bil+1):
-    #     if(bil%i==0):
-    #         count+=1
-    # if(count==2):
-    #     return"prime"
-    # else:
-    #     return"false"
+    for i in range(1,bil+1):
+        if(bil%i==0):
+            count+=1
+    if(count==2):
+        return"prime"
+    else:
+        return"false"
     # if(bil>1):
             # print("bukan")
             # else:
