f5ec529e7f96887f31d909a0513e3a71d757695f
ennyst <37688275+ennyst@users.noreply.github.com>
2018-03-25 22:51:17 +0700

    bisa

--- cari jalan.py
+++ cari jalan.py
@@ -1,71 +1,56 @@
+N = 7
 
 
-N=8
-
-#cetak langkah jalurnya
 def printSolution(sol):
     for i in sol:
         for j in i:
-            print (str(j) + " ",end="")
-        print("")
+            print(str(j) + " ", end="")
+        print(" ")
 
-#fungsi cek jika x, y valid
-#index fornya N*N Maze
 def isSafe(maze, x, y):
-    if x >= 0 and x < N and y >= 0 and y < N and maze[x][y] == 1:
+    if x >= 0 and x <= N and y >= 0 and y <= N and maze[x][y] == 1:
         return True
-
     return False
 
+def solveMaze(maze):
+    sol = [[0 for j in range(8)] for i in range(8)]
 
-def solveMaze (maze):
-    #matriks 8x8
-    sol = [[0 for j in range (8)] for i in range(8)]
-
-    if solveMazeUtil(maze, 0, 1, sol) == False:
-        print ("doesnt exist")
+    if solveMazeUtil(maze, input1, input2, sol) == False:
+        print("Solution doesn't exist");
         return False
-
     printSolution(sol)
     return True
 
-#fungsi pengecekan perlangkah
-def solveMazeUtil(maze,x, y, sol):
-    #titik koordinat finish
-    if x == N - 2 and y == N - 4:
-        sol[x][y] =1
+def solveMazeUtil(maze, x, y, sol):
+    if x == N - 1 and y == N - 3:
+        sol[x][y] = "x"
         return True
 
-#cek jiak maze[x][y] valid
-    if isSafe(maze,x,y) == True:
-        sol[x][y] =1 #nandain kalo koordinat merupakan path benar
+    if isSafe(maze, x, y) == True:
+        sol[x][y] = "z"
 
-        #pindah ke bawah
-        if solveMazeUtil(maze, x+1, y, sol) == True:
+        if solveMazeUtil(maze, x + 1, y, sol) == True:
             return True
 
-        # jika kebawah tidak bisa, maka pindah ke kanan
         if solveMazeUtil(maze, x, y + 1, sol) == True:
             return True
 
-        #jika kekanan dan kebawah tidak bisa, maka pindah ke kiri
-        if solveMazeUtil(maze, x, y-1, sol)== True:
+        if solveMazeUtil(maze, x, y - 1, sol) == True:
             return True
-
-        #x,y bukan jalurnya
-        sol[x][y] = 0
         return False
 
-if __name__ == "_____main____":
-    maze = {[1,1,1,0,1,1,1,1],
-            [1,1,0,0,0,1,1,0],
-            [1,1,1,1,1,1,1,0],
-            [1,1,1,1,1,1,1,0],
-            [0,0,0,1,1,1,1,0],
-            [1,1,1,0,0,1,1,0],
-            [1,1,1,1,1,1,0,1],
-            [1,1,1,1,0,0,1,1]}
+if __name__ == "__main__":
+    maze = [[1, 1, 1, 0, 1, 1, 1, 1],
+            [1, 1, 0, 0, 0, 1, 1, 0],
+            [1, 1, 1, 1, 1, 1, 1, 0],
+            [1, 1, 1, 1, 1, 1, 1, 0],
+            [0, 0, 0, 1, 1, 1, 1, 0],
+            [1, 1, 1, 0, 0, 1, 1, 0],
+            [1, 1, 1, 1, 1, 1, 0, 1],
+            [1, 1, 1, 1, 0, 0, 1, 1]]
 
+    input1 = int(input("masukan input 0-3 : "))
+    input2 = int(input("masukan input2 0-7 : "))
     solveMaze(maze)
 
 
